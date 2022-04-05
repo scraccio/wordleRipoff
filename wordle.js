d109a28ddcb3e4b2d04a88ee52a8f82ea392efba
scraccio <95088305+scraccio@users.noreply.github.com>
2022-04-05 17:10:30 +0200

    Update wordle.js

--- wordle.js
+++ wordle.js
@@ -111,3 +111,80 @@ function logKey(e) {
         string = '';
     }
 }
+
+function keyClick(e) {
+    curr = (j+1).toString() + (i+1).toString();
+    if(e == 'Backspace'){
+        if(i>0){
+            i-=1;
+        }
+        curr = (j+1).toString() + (i+1).toString();
+        element = document.getElementById(curr);
+        element.textContent = '' ;
+        string = string.slice(0, -1);
+        //console.log(string);
+        
+    }
+    if(e == 'Enter' && i==5 && j<=5 && (accepted.includes(string) || dictionary.includes(string))) {
+        if(string == randWord){
+            for(let i=0; i<5; i++){
+                if(string[i] == randWord[i]){
+                    let curr1 = (j+1).toString() + (i+1).toString();
+                    //console.log(curr1);
+                    elementX = document.getElementById(curr1);
+                    elementX.style.backgroundColor = "green";
+                }
+            }
+            element = document.getElementById("title");
+            element.textContent = element.textContent.replace("WordleRipoff","GODO");
+            console.log("GODO");
+            finish=true;
+        }
+        else{
+            if(j==5){
+                element = document.getElementById("title");
+                element.textContent = "The word was: " + randWord +  " :(";
+            }
+            for(let x=0; x<5; x++){
+                for(let y=0; y<5; y++){
+                    if(randWord[y] == string[x]){
+                        let curr1 = (j+1).toString() + (x+1).toString();
+                        //console.log(curr1);
+                        elementX = document.getElementById(curr1);
+                        if(x==y){
+                            elementX.style.backgroundColor = "green";
+                        }
+                        else{
+                            elementX.style.backgroundColor = "rgb(200,170,0)";
+                        }
+                        
+                    }
+                }
+            }
+
+            i=0;
+            j+=1;
+            string = '';
+            //curr = (j+1).toString() + (i+1).toString();
+            console.log(i);
+        }
+        if(j==6){
+            element = document.getElementById("retry");
+            element.textContent = "Press 'r' to retry";
+            finish=true;
+            console.log(e);
+        }
+
+    }
+
+    if(e != 'Backspace' && e != 'Enter' && i<=4 && j<6){
+        element = document.getElementById(curr);
+        element.textContent = e;
+        i+=1;
+        curr = (j+1).toString() + (i+1).toString();
+        string += e.toLowerCase();
+        //console.log(string);
+    }
+    //console.log(i);
+}
+
